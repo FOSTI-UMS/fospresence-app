e98c47d869f1c2acb9f250ebd42a99adbf638026
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2024-03-03 16:38:27 +0700

    integrating firebase into the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fospresence/core/themes/light_theme.dart';
 import 'package:fospresence/features/home/presentation/pages/home.dart';
+import 'firebase_options.dart';
 
 import 'core/services/injection_container.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await init();
   runApp(const MyApp());
 }
