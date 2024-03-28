8ef7fb636266cf09a672169a2dc2150434b6736f
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2024-03-28 10:26:59 +0700

    add a feature to select a role before exporting data to excel

--- lib/config/themes/light_theme.dart
+++ lib/config/themes/light_theme.dart
@@ -7,7 +7,7 @@ ThemeData get lightThemeData => ThemeData(
       appBarTheme: AppBarTheme(
         scrolledUnderElevation: 0,
         backgroundColor: primaryLightColor,
-        foregroundColor: Color.fromARGB(255, 34, 34, 34),
+        foregroundColor: const Color.fromARGB(255, 34, 34, 34),
         centerTitle: true,
         titleTextStyle: const TextStyle(
           fontFamily: "Nunito",
